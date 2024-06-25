6b901ab354ffbba73259cd58e9f203c77ab8f2a2
Jason Ansel <jansel@meta.com>
2024-06-25 13:56:49 -0700

    [halide-backend] Disable split reductions for Halide
    ghstack-source-id: 61272dd3e72d9d32209d4c184b2f4ca60ba8a351
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129320

--- torch/_inductor/codegen/common.py
+++ torch/_inductor/codegen/common.py
@@ -153,6 +153,7 @@ class BackendFeature(Enum):
     TUPLE_REDUCTION = auto()
     PREFER_STORE_LOOP_ORDER = auto()
     TRITON_TEMPLATES = auto()
+    REDUCE_TO_SINGLE_ELEMENT = auto()
 
 
 def get_backend_features(device: Union[torch.device, str]):
