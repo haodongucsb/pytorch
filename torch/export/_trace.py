404920d4cc80e8e1f0b43dabd913373b905d2fd5
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2024-06-25 09:39:02 -0700

    Don't decompose functional composite ops in export inference IR
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128077
    
    Recently we decided to split export IR into two different IRs (training vs inference). In the inference IR, one major change we decided to introduce was we wanted to keep the composite ops that user specified in the IR. This PR does that by overriding the CompositeImplicitAutograd decomp in export inference path.
    
    @imported-using-ghimport
    
    Differential Revision: [D58701607](https://our.internmc.facebook.com/intern/diff/D58701607/)
    ghstack-source-id: 056dd9439ca3f238d5e06a665cfb10877f1d260d

--- torch/export/_trace.py
+++ torch/export/_trace.py
@@ -1504,6 +1504,7 @@ def _export(
 
     # Call the appropriate export function based on the strictness of tracing.
     export_func = _strict_export if strict else _non_strict_export
+
     export_artifact = export_func(
         mod,
         args,
@@ -1517,7 +1518,6 @@ def _export(
         _disable_forced_specializations,
         _is_torch_jit_trace,
     )
-
     # Decompose here for readability.
     gm = export_artifact.aten.gm
     export_graph_signature = export_artifact.aten.sig
