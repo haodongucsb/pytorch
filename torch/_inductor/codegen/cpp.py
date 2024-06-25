ef0400c3ae81069ade356d4d2156561b4e08e46e
leslie-fang-intel <leslie.fang@intel.com>
2024-06-24 22:00:28 -0700

    [Inductor][CPP] Support more than one LocalBuffer
    ghstack-source-id: 6a923b167e388527d66c3bf652fb7eee9463ac5c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129121

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -7,7 +7,6 @@ import logging
 import math
 import re
 import sys
-from collections import namedtuple
 from copy import copy, deepcopy
 from enum import Enum
 from typing import Any, cast, Dict, List, Optional, Sequence, Set, Tuple, Union
@@ -2031,26 +2030,25 @@ class CppKernel(Kernel):
                 ):
                     # Allocate local buffer
                     local_buffers = V.local_buffer_context.local_buffers
-                    assert len(local_buffers.items()) == 1
-                    local_buffer = next(iter(local_buffers.items()))[1]
-                    # For dynamic size, rename s to ks
-                    local_buf_size = sympy_product(
-                        [
-                            self.rename_indexing(size_val)
-                            for size_val in local_buffer.get_layout().size
-                        ]
-                    )
-                    local_buf_dtype = DTYPE_TO_CPP[local_buffer.get_layout().dtype]
-                    allocate = (
-                        f"std::make_unique<{local_buf_dtype} []>({local_buf_size})"
-                    )
-                    code.splice(
-                        f"std::unique_ptr<{local_buf_dtype} []> local_buffer = {allocate};"
-                    )
-                    local_buffer_name = local_buffer.get_name()
-                    code.splice(
-                        f"{local_buf_dtype}* {local_buffer_name} = local_buffer.get();"
-                    )
+                    for local_buffer in local_buffers.values():
+                        # For dynamic size, rename s to ks
+                        local_buf_size = sympy_product(
+                            [
+                                self.rename_indexing(size_val)
+                                for size_val in local_buffer.get_layout().size
+                            ]
+                        )
+                        local_buf_dtype = DTYPE_TO_CPP[local_buffer.get_layout().dtype]
+                        allocate = (
+                            f"std::make_unique<{local_buf_dtype} []>({local_buf_size})"
+                        )
+                        local_buffer_name = local_buffer.get_name()
+                        code.splice(
+                            f"std::unique_ptr<{local_buf_dtype} []> buf_{local_buffer_name} = {allocate};"
+                        )
+                        code.splice(
+                            f"{local_buf_dtype}* {local_buffer_name} = buf_{local_buffer_name}.get();"
+                        )
                 gen_loops(loop_nest.root)
             else:
                 gen_kernel(loop_nest.kernel)
@@ -3857,8 +3855,9 @@ class CppScheduling(BaseScheduling):
                 call_ranges = tuple(group) + tuple(reduction_group)
                 return call_ranges
 
-            LocalBuffer = namedtuple("LocalBuffer", ["local_buf", "global_buf"])
-            local_buffers: List[LocalBuffer] = []
+            local_buffers: List[ir.Buffer] = []
+            # Map local buffer name to a list of global buffers
+            local_to_global_buffers: Dict[str, List[ir.Buffer]] = {}
             if all(
                 len(get_call_ranges(_node)) == node.outer_loop_fusion_depth + 1
                 for _node in node.get_outer_nodes()
@@ -3888,23 +3887,39 @@ class CppScheduling(BaseScheduling):
                             global_buffer_layout.size[size_offset:],
                             global_buffer_layout.stride[size_offset:],
                         )
-                        local_buffers.append(
-                            LocalBuffer(
-                                local_buf=ir.Buffer(
-                                    "local_buffer_data", local_buffer_layout
-                                ),
-                                global_buf=global_buffer,
-                            )
+
+                        def try_share_local_buffer(local_buffer_layout, local_buffers):
+                            shared_buffers = [
+                                local_buf
+                                for local_buf in local_buffers
+                                if local_buffer_layout == local_buf.layout
+                            ]
+                            return shared_buffers[0] if shared_buffers else None
+
+                        local_buf_prefix = "local_buffer_data"
+                        # Share existing local buffer
+                        local_buffer_used = try_share_local_buffer(
+                            local_buffer_layout, local_buffers
                         )
-                        # At most 1 node with local buf for each OuterLoopFusedSchedulerNode
-                        break
-            assert len(local_buffers) in [0, 1]
+                        if not local_buffer_used:
+                            # Create new local buffer
+                            local_buffer_used = ir.Buffer(
+                                f"{local_buf_prefix}_{len(local_buffers)}",
+                                local_buffer_layout,
+                            )
+                            local_buffers.append(local_buffer_used)
+                            local_to_global_buffers[local_buffer_used.name] = []
+                        local_to_global_buffers[local_buffer_used.name].append(
+                            global_buffer,
+                        )
 
             with LocalBufferContext(kernel_group.args) as scope:
                 if len(local_buffers) > 0:
-                    scope.add_local_buffer(
-                        local_buffers[0].local_buf, local_buffers[0].global_buf
-                    )
+                    for local_buffer in local_buffers:
+                        assert local_buffer.name is not None
+                        scope.add_local_buffer(
+                            local_buffer, local_to_global_buffers[local_buffer.name]
+                        )
                 for _node in node.get_outer_nodes():
                     assert isinstance(_node, (FusedSchedulerNode, SchedulerNode))
                     cpp_kernel_proxy = CppKernelProxy(kernel_group)
@@ -3919,7 +3934,7 @@ class CppScheduling(BaseScheduling):
                 metrics.cpp_outer_loop_fused_inner_counts.append(
                     metrics.CppOuterLoopFusedCount(
                         len(cpp_kernel_proxy_list),
-                        local_buffer_number=len(local_buffers),
+                        local_buffer_number=len(list(scope.local_buffers.values())),
                     )
                 )
                 outer_fusion_cpp_kernel_proxy = node.merge_outer_fusion_kernels(
