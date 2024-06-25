510ab0fe4e6931c524ce7c5d9787716a8baf6e0c
Chirag Pandya <cpio@meta.com>
2024-06-25 15:33:33 -0700

    Flight recoder data as JSON
    Summary:
    Provide a new API to retrieve flight recorder data as JSON.
    The one minor different between flight recorder as Pickle v/s JSON is
    that the JSON API does not retrieve stack traces at the moment.
    This ends up being far too much data.
    
    Test Plan:
    unit test
    
    ghstack-source-id: 9996c3840665a367c9cf1ed8abe607d09e05027b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129505

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -342,10 +342,11 @@ void cacheAllocatorDeregisterHook(
 }
 
 #if defined(IS_NCCLX) && defined(NCCL_COMM_DUMP)
-std::string dump_nccl_trace(
+std::string dump_nccl_trace_helper(
     bool includeCollectives,
     bool includeStackTraces,
-    bool onlyActive) {
+    bool onlyActive,
+    bool json) {
   std::unordered_map<
       std::string /* ncclUniqueID */,
       std::unordered_map<std::string, std::string> /* dump from this comm */>
@@ -365,19 +366,44 @@ std::string dump_nccl_trace(
     std::string ncclUniqueIDStr = buildNcclUniqueIdStr(ncclComm->getNcclId());
     ncclDumpMap[ncclUniqueIDStr] = ncclComm->ncclCommDump();
   }
+  if (json) {
+    return NCCLTraceBuffer::get()->dump_json(
+        ncclDumpMap, includeCollectives, onlyActive);
+  }
   return NCCLTraceBuffer::get()->dump(
       ncclDumpMap, includeCollectives, includeStackTraces, onlyActive);
 }
 
 #else
-std::string dump_nccl_trace(
+std::string dump_nccl_trace_helper(
     bool includeCollectives,
     bool includeStackTraces,
-    bool onlyActive) {
+    bool onlyActive,
+    bool json) {
+  if (json) {
+    return NCCLTraceBuffer::get()->dump_json(
+        std::nullopt, includeCollectives, onlyActive);
+  }
   return NCCLTraceBuffer::get()->dump(
       c10::nullopt, includeCollectives, includeStackTraces, onlyActive);
 }
 #endif
+
+std::string dump_nccl_trace(
+    bool includeCollectives,
+    bool includeStackTraces,
+    bool onlyActive) {
+  return dump_nccl_trace_helper(
+      includeCollectives, includeStackTraces, onlyActive, false /* json */);
+}
+
+std::string dump_nccl_trace_json(bool includeCollectives, bool onlyActive) {
+  return dump_nccl_trace_helper(
+      includeCollectives,
+      false /* includeStackTraces */,
+      onlyActive,
+      true /* json */);
+}
 
 std::optional<std::function<void(std::function<void(const std::string&)>)>>&
 get_cpp_trace_dumper() {
